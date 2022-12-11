64709e1c2d6f465f1f162b7336426a22c98dcb8a
Jose Sanchez <53316023+jsanchezalmagro@users.noreply.github.com>
2022-12-11 10:56:53 +0000

    Update arc

--- rcosdesign.py
+++ rcosdesign.py
@@ -1,2 +1,94 @@
-<html> <head> <title>Binder not found</title>
-<link href="https://binder.mybinder.ovh/static/dist/styles.css" rel="stylesheet"> </head> <body> <div class="container"> <h2 class="text-center">Oops!</h2> <h3 class="text-center">We can't seem to find the Binder page you are looking for.</h3> <h4>424 error</h4> <p> Here are some helpful tips. </p> <p> <h4>Is this a Binder that you created?</h4> <p> Your Binder stopped due to an error or it was removed due to age or inactivity. </p> <p> Return to the <a href="https://binder.mybinder.ovh">Binder home page</a> to retry creating your Binder. </p> <h4>Did someone give you this Binder link?</h4> <p> If so, the link is outdated or incorrect. Recheck the link for typos or ask the person who gave you the link for an updated link. A shareable Binder link should look like <tt>https://binder.mybinder.ovh/v2/gh/...</tt>. </p> <h4><a href="https://binder.mybinder.ovh">Binder home page</a></h4> </body>
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def rcosdesign(beta: float, span: float, sps: float, shape='normal'):
+    """ Raised cosine FIR filter design
+    Calculates square root raised cosine FIR
+    filter coefficients with a rolloff factor of `beta`. The filter is
+    truncated to `span` symbols and each symbol is represented by `sps`
+    samples. rcosdesign designs a symmetric filter. Therefore, the filter
+    order, which is `sps*span`, must be even. The filter energy is one.
+
+    Keyword arguments:
+    beta  -- rolloff factor of the filter (0 >= beta <= 1)
+    span  -- number of symbols that the filter spans
+    sps   -- number of samples per symbol
+    shape -- `normal` to design a normal raised cosine FIR filter or
+             `sqrt` to design a sqre root raised cosine filter
+    """
+
+    if beta < 0 or beta > 1:
+        raise ValueError("parameter beta must be float between 0 and 1, got {}".format(beta))
+
+    if span < 0:
+        raise ValueError("parameter span must be positive, got {}"
+                         .format(span))
+
+    if sps < 0:
+        raise ValueError("parameter sps must be positive, got {}".format(span))
+
+    if ((sps*span) % 2) == 1:
+        raise ValueError("rcosdesign:OddFilterOrder {}, {}".format(sps, span))
+
+    if shape != 'normal' and shape != 'sqrt':
+        raise ValueError("parameter shape must be either 'normal' or 'sqrt'")
+
+    eps = np.finfo(float).eps
+
+    # design the raised cosine filter
+
+    delay = span*sps/2
+    t = np.arange(-delay, delay)
+
+    if len(t) % 2 == 0:
+        t = np.concatenate([t, [delay]])
+    t = t / sps
+    b = np.empty(len(t))
+
+    if shape == 'normal':
+        # design normal raised cosine filter
+
+        # find non-zero denominator
+        denom = (1-np.power(2*beta*t, 2))
+        idx1 = np.nonzero(np.fabs(denom) > np.sqrt(eps))[0]
+
+        # calculate filter response for non-zero denominator indices
+        b[idx1] = np.sinc(t[idx1])*(np.cos(np.pi*beta*t[idx1])/denom[idx1])/sps
+
+        # fill in the zeros denominator indices
+        idx2 = np.arange(len(t))
+        idx2 = np.delete(idx2, idx1)
+
+        b[idx2] = beta * np.sin(np.pi/(2*beta)) / (2*sps)
+
+    else:
+        # design a square root raised cosine filter
+
+        # find mid-point
+        idx1 = np.nonzero(t == 0)[0]
+        if len(idx1) > 0:
+            b[idx1] = -1 / (np.pi*sps) * (np.pi * (beta-1) - 4*beta)
+
+        # find non-zero denominator indices
+        idx2 = np.nonzero(np.fabs(np.fabs(4*beta*t) - 1) < np.sqrt(eps))[0]
+        if idx2.size > 0:
+            b[idx2] = 1 / (2*np.pi*sps) * (
+                np.pi * (beta+1) * np.sin(np.pi * (beta+1) / (4*beta))
+                - 4*beta           * np.sin(np.pi * (beta-1) / (4*beta))
+                + np.pi*(beta-1)   * np.cos(np.pi * (beta-1) / (4*beta))
+            )
+
+        # fill in the zeros denominator indices
+        ind = np.arange(len(t))
+        idx = np.unique(np.concatenate([idx1, idx2]))
+        ind = np.delete(ind, idx)
+        nind = t[ind]
+
+        b[ind] = -4*beta/sps * (np.cos((1+beta)*np.pi*nind) +
+                                np.sin((1-beta)*np.pi*nind) / (4*beta*nind)) / (
+                                np.pi * (np.power(4*beta*nind, 2) - 1))
+
+    # normalize filter energy
+    b = b / np.sqrt(np.sum(np.power(b, 2)))
+    return b
